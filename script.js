4bb22bd2ec84c983655c6b5d416fe6a4f18e8360
sjswan13 <sjswan13@gmail.com>
2024-07-12 16:23:39 -0600

    local storage use

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const playSelect = document.getElementById('play');
 
 let ticketPrice = +playSelect.value;
 
+//Save Selected movie index and price
+function setPlayData(playIndex, playPrice) {
+  localStorage.setItem('selectedPlayIndex', playIndex);
+  localStorage.setItem('selectedPlayPrice', playPrice);
+}
+
 //update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) =>
+    [...seats].indexOf(seat)
+  );
+  
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatCount = selectedSeats.length;
   
@@ -18,7 +30,8 @@ function updateSelectedCount() {
 
 //Play select Event
 playSelect.addEventListener('change', e => {
-  ticketPrice = +e.target.value
+  ticketPrice = +e.target.value;
+  setPlayData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
 
